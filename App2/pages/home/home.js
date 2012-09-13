860b8c7932cdfec6f957463c4a6b17c82aa93d55
vanbungkring <arie@wego.com>
2012-09-14 00:42:11 +0700

    working DrawDeals() now

--- App2/pages/home/home.js
+++ App2/pages/home/home.js
@@ -5,8 +5,12 @@
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
-   
+            initDeals()
             // TODO: Initialize the page here.
         }
     });
 })();
+
+function DrawDeals() {
+   
+}
