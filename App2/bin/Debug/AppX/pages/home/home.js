d1079d1ceff0f277d6b7056f42dcd28b6d11a714
vanbungkring <arie@wego.com>
2012-09-13 22:27:36 +0700

    adding image on searchbox title

--- App2/bin/Debug/AppX/pages/home/home.js
+++ App2/bin/Debug/AppX/pages/home/home.js
@@ -5,6 +5,10 @@
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
+            Button1.addEventListener('click', function (e) {
+                var msg = new Windows.UI.Popups.MessageDialog("GoodBye, see you soon!");
+                msg.showAsync().then();
+            })
             // TODO: Initialize the page here.
         }
     });
--- App2/pages/home/home.js
+++ App2/pages/home/home.js
@@ -5,6 +5,7 @@
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
+   
             // TODO: Initialize the page here.
         }
     });
