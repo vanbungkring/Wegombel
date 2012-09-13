febed00fddea3d206441ace10277d8b347f006be
vanbungkring <arie@wego.com>
2012-09-14 01:52:28 +0700

    Stuck On Wego Deals... will finish it today :)

--- App2/bin/Debug/AppX/pages/home/home.js
+++ App2/bin/Debug/AppX/pages/home/home.js
@@ -5,13 +5,21 @@
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
-            initDeals()
+            DrawDeals()
             // TODO: Initialize the page here.
         }
     });
 })();
 
-function initDeals() {
-    var msg = new Windows.UI.Popups.MessageDialog("GoodBye, see you soon!");
-    msg.showAsync().then();
+function DrawDeals() {
+    var url = 'http://deals.wego.com/api/featured/XX.js?currency=USD';
+    WinJS.xhr({ url: url }).then(function (data) {
+        var result = JSON.parse(data.responseText);
+        for (var i = 0; i < result.length; i++) {
+            var greetingString = result[i].url;
+            console.log(greetingString);
+        }
+        document.getElementById("AU").innerText = greetingString;
+    });
+
 }
--- App2/pages/home/home.js
+++ App2/pages/home/home.js
@@ -5,12 +5,20 @@
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
-            initDeals()
+            DrawDeals()
             // TODO: Initialize the page here.
         }
     });
 })();
 
 function DrawDeals() {
-   
+    var url = 'http://deals.wego.com/api/featured/XX.js?currency=USD';
+    WinJS.xhr({ url: url }).then(function (data) {
+        var result = JSON.parse(data.responseText);
+        for (var i = 0; i < result.length; i++) {
+            var greetingString = result[i].url;
+        }
+        document.getElementById("AU").innerText = greetingString;
+    });
+
 }
