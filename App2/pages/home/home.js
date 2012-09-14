3686a1950556877838398f976cf44467b8076699
vanbungkring <arie@wego.com>
2012-09-14 15:14:53 +0700

    add and remove function on home.js

--- App2/pages/home/home.js
+++ App2/pages/home/home.js
@@ -5,13 +5,8 @@
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
-            try {
-                DrawDeals()
-            }
-            catch (e) {
+            DrawDeals()
 
-            }
-            // TODO: Initialize the page here.
         }
     });
 })();
