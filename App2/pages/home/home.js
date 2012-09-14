665638c118128ae78324ed32a6a857dd0e824ad0
vanbungkring <arie@wego.com>
2012-09-14 14:01:13 +0700

    adding class on deals

--- App2/pages/home/home.js
+++ App2/pages/home/home.js
@@ -23,7 +23,7 @@ function DrawDeals() {
         var yyy = document.getElementsByClassName("deals-draw");
         for (var i = 0; i < result.length; i++) {
             result[i].url;
-            document.getElementById("deals-draw").innerHTML += '<li><a href=' + result[i].url + '><img class="deals-image" src=' + result[i].image + '></li>';
+            document.getElementById("deals-draw").innerHTML += '<li><a href=' + result[i].url + '><img class="deals-image" src=' + result[i].image + '> <p>' + result[i].headline + '</p></a></li>';
         }
 
 
